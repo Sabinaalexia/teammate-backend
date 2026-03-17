e4e88d8f4af35fc13b247f484bec559001977480
Sabina Podina <sabinaalexia2004@yahoo.com>
2026-03-18 00:15:30 +0200

    Initial commit - Spring Boot backend setup

---
+++ src/main/java/com/studentprojects/teammate/TeammateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studentprojects.teammate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentprojects/teammate/TeammateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentprojects.teammate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeammateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
